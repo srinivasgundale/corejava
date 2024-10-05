e97b42c0e2cd4d997ae3adf90e6523bd3eea134d
Srinivas Gundali <srinivasgundale@gmail.com>
2024-10-06 00:05:56 +0800

    OOPS : Classes and Objects

---
+++ Car.java
@@ -0,0 +1,17 @@
+class Car {
+    String model;
+    int year;
+
+    // Constructor
+    public Car(String model, int year) {
+        this.model = model;
+        this.year = year;
+    }
+
+    // Method to display car details
+    public void displayInfo() {
+        System.out.println("Model: " + model + ", Year: " + year);
+    }
+}
+
+
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car("Toyota", 2020); // Object instantiation
+        car1.displayInfo(); // Calling method
+    }
+}
