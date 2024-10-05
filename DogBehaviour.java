b1e9a7b7264f241494f626d9d6ca035f2f045c05
Srinivas Gundali <srinivasgundale@gmail.com>
2024-10-06 00:21:22 +0800

    Exception Handling

--- DogBehaviour.java
+++ DogBehaviour.java
@@ -1,5 +1,14 @@
 class DogBehaviour extends Animal {
     public void sound() {
         System.out.println("Dog barking");
+        try {
+            int result = 10 / 0;  // ArithmeticException
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            System.out.println("This will always execute.");
+        }
+        
     }
 }
