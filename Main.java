59cab9ccd498b0b30924aa38ca940781f54dae50
Srinivas Gundali <srinivasgundale@gmail.com>
2024-10-06 00:22:23 +0800

    Collections Framework

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Car car1 = new Car("Toyota", 2020); // Object instantiation
@@ -13,5 +14,13 @@ public class Main {
 
         DogBehaviour behaviour = new DogBehaviour();
         behaviour.sound();
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Alice");
+        names.add("Bob");
+
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
