1217c2bfa977a91d4a015ac186e3c55a3d54adfa
Srinivas Gundali <srinivasgundale@gmail.com>
2024-10-06 00:34:56 +0800

    lamdas

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-
+import java.util.List;
+import java.util.Optional;
 //file write
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,6 +13,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
+        
         Car car1 = new Car("Toyota", 2020); // Object instantiation
         car1.displayInfo(); // Calling method
 
@@ -63,5 +66,17 @@ public class Main {
         MyThread thread = new MyThread();
         thread.start();
 
+        List<String> names1 = Arrays.asList("John", "Jane", "Doe");
+        names1.forEach(name -> System.out.println(name));
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        numbers.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
+
+        
+        
+
     }
+    public static void getName() {
+        System.out.println("name is sri");
+    };
 }
